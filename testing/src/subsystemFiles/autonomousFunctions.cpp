41fa285dc2e56070f7335976faecaea4192e4f23
Flawlesssharky6 <Chengqianglin512@gmail.com>
2024-10-21 11:57:55 -0600

    auton update

--- testing/src/subsystemFiles/autonomousFunctions.cpp
+++ testing/src/subsystemFiles/autonomousFunctions.cpp
@@ -1,28 +1,31 @@
 #include "main.h"
 
 void redLeftCorner(){
+    intakePneumatic.set_value(true);
     //this is for contesting the middle stake
-    translate(60, 127); //go to center
+    translate(22, 127); //go to center
     clampPneumatic.set_value(true); //clamp on stake
     conveyorIntake(3000); //put ring on the stake
-    translate(10, -127); //bakc up a little bit
-    rotate(30, 60);// turn to face the corner
-    translate(60, 127); //drive into corner
+    translate(2, -127); //bakc up a little bit
+    //rotate(30, 60);// turn to face the corner
+    //translate(30, 127); //drive into corner
     clampPneumatic.set_value(false); //release stake
-    translate(120, 127); // drive to climbing bars  
+    rotate(45, 100);//face climbing bars
+    translate(6, 127); // drive to climbing bars  
 }
 void redRightCorner(){
-    translate(30, 127); //drive to stake
+    translate(13, 127); //drive to stake
     clampPneumatic.set_value(true); //clamp on stake
     conveyorIntake(3000); //put ring on the stake
-    translate(10, 60); //drive forward a little
+    translate(1, 60); //drive forward a little
     rotate(90, 100); //trun to face the ring
-    translate(30, 127);
+    translate(8, 127);
     intakePneumatic.set_value(true); //set intake down
     conveyorIntake(3000); // intake ring
-    rotate(180, 127);
     clampPneumatic.set_value(false); // let of stake
-    translate(70, 127); // move to climbing bars
+    rotate(180, 127);
+    
+    translate(16, 127); // move to climbing bars
 }
 void blueLeftCorner(){
     //example code
